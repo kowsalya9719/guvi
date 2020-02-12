80d1d22fe36411b2c668fb82f578d91dfc42ce93
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-12 23:54:50 +0530

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,17 @@
+#calculator
+
+a=int(input("enter value 1 : "))
+b=int(input("enter value 2 : "))
+option=int(input("enter your option: "))
+if(option==1):
+    print("solution : ",a+b)
+elif(option==2):
+    print("solution : ",a-b)
+elif(option==3):
+    print("solution : ",a*b)
+elif(option==4):
+    print("solution : ",a%b)
+elif(option==5):
+    print("solution : ",a/b)
+elif(option==6):
+    print("solution : ",a^b)
