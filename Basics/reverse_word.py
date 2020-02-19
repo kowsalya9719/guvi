76c5fa7fde4db368023a0b07cd4a4cd4eda81380
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-19 10:24:05 +0530

    #Given a string S consisting of 2 words reverse the order of two words .

---
+++ Basics/reverse_word.py
@@ -0,0 +1,8 @@
+#Given a string S consisting of 2 words reverse the order of two words .
+
+s = input()
+words = s.split(' ') 
+string =[] 
+for word in words: 
+    string.insert(0, word) 
+print(" ".join(string)) 
