ab5417cac9c1e9dfec78ce66584cd0b2fd81a453
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-20 12:43:18 +0530

    #Given a string print reverse all words except the first and last words.

---
+++ string/reverse_first_last.py
@@ -0,0 +1,7 @@
+#Given a string print reverse all words except the first and last words.
+s=str(input())
+l=[s for s in s.split(' ')]
+print(l[0],end=" ")
+for i in range(1,len(l)-1):
+	print(l[i][::-1],end=" ")
+print(l[len(l)-1])    
