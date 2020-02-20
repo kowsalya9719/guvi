feef7449c7d5b47c44546c5942ba5bd4dfdb71f9
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-20 11:06:01 +0530

    #Prateek finds it difficult to judge the minimum element in the list of elements given to him. Your task is to develop the algorithm in order to find the minimum element.

---
+++ array/minimum_prateek.py
@@ -0,0 +1,5 @@
+#Prateek finds it difficult to judge the minimum element in the list of elements given to him. Your task is to develop the algorithm in order to find the minimum element.
+n=int(input())
+num=list(map(int,input().split()))
+print(min(num))
+
