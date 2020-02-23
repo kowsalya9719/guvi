68a9c9046e1870658f9b68d970b33de9af506d2e
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-23 23:21:14 +0530

    #A number is given as input. Find the odd digits in the number, add them and find if the sum is odd or not. If even print E, if odd print O

---
+++ array/odd_even.py
@@ -0,0 +1,14 @@
+#A number is given as input. Find the odd digits in the number, add them and find if the sum is odd or not. If even print E, if odd print O
+N=int(input())
+sum=0
+while(N>0):
+    n=N%10
+    sum=sum+n
+    N=N//10
+c=sum
+if(c%2==0):
+    print("E")
+else:
+    print("O")
+
+    
