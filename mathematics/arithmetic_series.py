7efab08f3051f76faa059b597812e5a274bd62a5
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-23 23:54:48 +0530

    #Given 3 numbers A,B,C find the sum of Arithmetic Series with a=A, d=B and n=C

---
+++ mathematics/arithmetic_series.py
@@ -0,0 +1,14 @@
+#Given 3 numbers A,B,C find the sum of Arithmetic Series with a=A, d=B and n=C
+A,B,C=map(int,input().split())
+a=A
+d=B
+n=C
+x=(n/2)*((2*a)+(n-1)*d)
+print(x)
+
+                  
+                  
+                  
+                  
+
+                  
