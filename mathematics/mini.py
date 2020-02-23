0c275b4c073683c9a4120d79662ba9bed85c9f6f
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-24 00:02:11 +0530

    #Pk finds it difficult to judge the minimum element in the list of elements given to him. Your task is to develop the algorithm in order to find the minimum element.

---
+++ mathematics/mini.py
@@ -0,0 +1,5 @@
+#Pk finds it difficult to judge the minimum element in the list of elements given to him. Your task is to develop the algorithm in order to find the minimum element.
+N=int(input())
+n1=list(map(int,input().split()))
+minimum=min(n1)
+print(minimum)
