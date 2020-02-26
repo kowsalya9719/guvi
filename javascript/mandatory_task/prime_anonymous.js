67796850e62d779f0a15767f65e4ac881e1e5697
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-26 12:55:45 +0530

    #return all prime numbers in an array(anonymous function)

---
+++ javascript/mandatory_task/prime_anonymous.js
@@ -0,0 +1,9 @@
+#return all prime numbers in an array(anonymous function)
+const Array = [1, 3, 2, 5, 10];
+const PrimeArray =Array.filter(function(num){
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+});
+console.log(PrimeArray);
