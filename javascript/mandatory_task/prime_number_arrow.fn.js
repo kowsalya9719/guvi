35afeebcbf93c8e9fd6e0d753d1d76298d2e8b45
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-02-26 12:54:13 +0530

    #return all prime number in an array(arrow function)

---
+++ javascript/mandatory_task/prime_number_arrow.fn.js
@@ -0,0 +1,9 @@
+#return all prime number in an array(arrow function)
+const newArray = [1, 3, 2, 5, 10];
+const myPrimeArray = newArray.filter(num => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+});
+console.log(myPrimeArray);
