d7e48f3ab7a8dc21fbed3dfe3586786544c50314
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-04-08 19:53:58 +0530

    //Check whether the given 4 points form a square or not.

---
+++ javascript/basics/square_or_not.js
@@ -0,0 +1,28 @@
+//Check whether the given 4 points form a square or not.
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+  m.push(data);
+});
+inp.on("close",()=>{
+    for(var i=0;i<m.length;i++){
+        var a=m[i].split(" ");
+        var b=parseInt(a[0]);
+        var c=parseInt(a[1]);
+        var prod=b*c;
+        var count=0;
+        if(prod/b==b){
+            count+=1;
+        }
+    }
+    if(count==m.length){
+        console.log("yes");
+    }
+    else{
+        console.log("no");
+    }
+
+});
