8d3215493986706cbf023b1722a013ab50a5198f
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-04-12 14:08:34 +0530

    //convert a string in to a ascii

---
+++ javascript/String/ascii.js
@@ -0,0 +1,15 @@
+//convert a string in to a ascii
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+    var S=data.split("");
+    var sum=0;
+    for(i=0;i<S.length;i++){
+        sum+=S[i].charCodeAt();
+
+    }
+    console.log(sum);
+});
