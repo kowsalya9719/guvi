95ad4a714df951ae3b8533e701b8eaa351735d53
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-04-16 00:00:04 +0530

    //Given a string S ,print the vowels first and then consonants in the same order as they have occurred in the string.

---
+++ javascript/String/vowels_first_consonant_second.js
@@ -0,0 +1,29 @@
+//Given a string S ,print the vowels first and then consonants in the same order as they have occurred in the string.
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+  m.push(data);
+});
+inp.on("close",()=>{
+  var S=m[0].split("");
+  var vowel="aeiou";
+  var c="";
+  var s="";
+  var v=vowel.split("");
+  for(i=0;i<S.length;i++){
+      var count=0;
+      for(j=0;j<v.length;j++){
+          if(S[i]==v[j]){
+              s+=S[i];
+              count=1;
+          }
+      }
+      if(count===0){
+          c+=S[i];
+      }
+  }
+  console.log(s+c);
+});
