ac962ea2c96ca92d17ff96b1e292c5cdb5185a4b
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-04-16 12:07:49 +0530

    //Given 2 strings S,X. Print 'yes' if their lengths are co-prime otherwise print 'no'.

---
+++ javascript/String/coprime.js
@@ -0,0 +1,32 @@
+//Given 2 strings S,X. Print 'yes' if their lengths are co-prime otherwise print 'no'.
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+  m.push(data);
+});
+inp.on("close",()=>{
+  var M=m[0].split(" ");
+  var S=M[0];
+  var X=M[1];
+  var s=S.length;
+  var x=X.length;
+  var count=0;
+  for(i=1;i<=s;i++)
+  {
+    if(s%i===0 && x%i===0)
+    {
+      count=count+1;
+    }
+  }
+  if(count==1)
+  {
+    console.log("yes")
+  }
+  else
+  {
+    console.log("no");
+  }
+});
