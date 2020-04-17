28c2b1519376a83dc216ed49d2bbadff7df88801
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-04-17 21:58:44 +0530

    //Given a range[L,R], print the sum of all the odd numbers within the range(inclusive of L and R)

---
+++ javascript/Numbers/sum_of_odd_numbers_in_range.js
@@ -0,0 +1,22 @@
+//Given a range[L,R], print the sum of all the odd numbers within the range(inclusive of L and R)
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+  m.push(data);
+});
+inp.on("close",()=>{
+  var a=m[0].split(" ");
+  var L=parseInt(a[0]);
+  var R=parseInt(a[1]);
+  var sum=0
+    for(i=L;i<=R;i++)
+    {
+      if(i%2!==0){
+        sum=sum+i;
+      }
+    }
+      console.log(sum);
+});
