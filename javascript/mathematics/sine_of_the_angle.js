8dd95d02772c173c367b67e4c0e3603ea1bb585f
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-04-30 12:58:29 +0530

    //Given an angle A, print the sine of the given angle.

---
+++ javascript/mathematics/sine_of_the_angle.js
@@ -0,0 +1,12 @@
+//Given an angle A, print the sine of the given angle.
+const readline = require('readline');
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+inp.on("line", (data) => {
+    var N=parseInt(data);
+	console.log(Math.sin(N* Math.PI / 180)); 
+
+});
