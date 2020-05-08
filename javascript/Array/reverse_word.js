ea36b7417e05c05940ffee36b9a99ae74503ec49
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-05-08 21:44:43 +0530

    //You are given with string of words,we have to arrange them in reverse saturated order.

---
+++ javascript/Array/reverse_word.js
@@ -0,0 +1,21 @@
+//You are given with string of words,we have to arrange them in reverse saturated order.
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+  var s=data.split(" ");
+  var S="";
+  var g=[];
+  for(var i=0;i<s.length;i++){
+      for(var j=s[i].length-1;j>=0;j--)
+      {
+          var a=s[i].split("");
+          S+=a[j];      
+      }
+      g.push(S);
+      S="";
+  }
+  console.log(g.join(" "));
+});
