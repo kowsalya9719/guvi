f7533112b0b22c4e3bef2b7c3165250138f1ef4b
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-01 22:53:27 +0530

    //1.convert specifications given below in image to javascript object.js

--- Tasks/Day1_(01-06-2020)/1.convert specifications given below in image to javascript object.js
+++ Tasks/Day1_(01-06-2020)/1.convert specifications given below in image to javascript object.js
@@ -1,1 +1,36 @@
 //1.convert specifications given below in image to javascript object.js
+var technicalspecifications={
+"Dimensions":[{"overall length":["mm","3500"]},
+              {"overall width":["mm","1600"]},
+              {"overall height":["mm","1490"]},
+              {"wheel base":["mm","2360"]},
+              {"track width":[{"front":["mm","1405"]},{"rear":["mm","1405"]}]},
+              {"minimum turning radius":["m","4.5"]},
+              {"minimum ground clearance":["mm","170"]}],
+
+"Capacities":[{"seating capacity":["persons","5"]},
+              {"Fuel tank capacity":["litres","35"]}],
+
+"Engine":[{"Type":"KB-Series"},
+          {"number of cylinders":"3"},
+          {"number of valves":"12"},
+          {"capacity":["cc/cm3","998"]},
+          {"bore x stroke":["mm","73.0x79.5"]},
+          {"compression ratio":"10:1"},
+          {"maximum power":["ps/rpm","67/6200"]},
+          {"maximum torque":["np/rpm","90/3500"]},
+          {"Fuel distribution":"multipoint injection"}],
+
+"Transmission":{"type":"5-speed MT"},
+
+"Chassls":[{"steering":"Rack & Pinion,Power assisted"},
+           {"Brakes":[{"front":"Ventilates discs"},{"rear":"Drums"}]},
+           {"Suspension":[{"front":"MacPherson strut & coil spring"},{"Rear":"isolated trailing link & coil spring"}]},
+           {"shock absorbers":"gas filled"},
+           {"shock absorbers":"gas filled"},
+           {"Tyre(tubeless)":"155/80r13"}],
+
+"Weights":[{"kerb weight(min,with full options)":["kg","860-880"]},
+              {"Gross vehicle weight":["kg","1,320"]}]
+
+}
