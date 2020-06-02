08ab8d9fe0e1ffd8357f00153033cb8c0ca6a91e
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-02 23:56:31 +0530

    //Update the fur color of bar

---
+++ Day 2:(02-06-2020)/2.8 Problem 0 : Part A-8.js
@@ -0,0 +1,26 @@
+//Update the fur color of bar
+function furcolor()
+{
+var cat = {
+ name: "Fluffy",
+ activities: ["play", "eat cat food"],
+ catFriends: [
+ {
+ name: "bar",
+ activities: ["be grumpy", "eat bread omblet"],
+ weight: 8,
+ furcolor: "white"
+ }, 
+ {
+ name: "foo",
+ activities: ["sleep", "pre-sleep naps"],
+ weight: 3
+ }
+ ]
+}
+//Update the fur color of bar
+cat.catFriends[0].furcolor="grey";
+console.log(cat);
+
+}
+furcolor();
