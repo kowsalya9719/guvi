c25ae25daae2fee32274900c36bd3ce79fb6e6cf
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 23:41:05 +0530

    //problem 5

---
+++ Day 3_(03-06-2020)/1.js/problem 5.js
@@ -0,0 +1,13 @@
+Problem
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+Examples
+nextNumber(0) ➞ 1
+nextNumber(9) ➞ 10
+nextNumber(-3) ➞ -2
+
+var myint = 0;
+function nextNumber(myint) {
+return myint+1
+}
+var myNextint = nextNumber(myint)
+console.log(myNextint)
