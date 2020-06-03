904555a475687507311ab6a0d7817b51da8becfb
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 22:27:10 +0530

    Problem 1

---
+++ Day 3_(03-06-2020)/1.js/Problem 1.js
@@ -0,0 +1,22 @@
+Problem 1:
+
+/*Write a function called “addFive”.
+Given a number, “addFive” returns 5 added to that number.
+Input:
+addFive(5);
+addFive(0);
+addFive(-5);*/
+
+var num = 10;
+function addFive(num)
+{ 
+num=num+5;
+return num;
+}
+var result = addFive(num)
+console.log(result);
+
+
+
+
+
