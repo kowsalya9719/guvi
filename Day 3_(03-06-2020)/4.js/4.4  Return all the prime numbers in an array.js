d5939f504a6a0aa270cf8f33d9b1eaa42222888a
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 21:45:26 +0530

    // Return all the prime numbers in an array

---
+++ Day 3_(03-06-2020)/4.js/4.4  Return all the prime numbers in an array.js
@@ -0,0 +1,9 @@
+// Return all the prime numbers in an array
+const newArray = [1, 3, 2, 5, 10];
+const myPrimeArray = newArray.filter(num => {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+});
+console.log(myPrimeArray);
