106133b71a91f8319e3f9dc8e1ed1cb2c18af408
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 21:42:47 +0530

    // Convert all the strings to title caps in a string array

---
+++ Day 3_(03-06-2020)/4.js/4.2 Convert all the strings to title caps in a string array .js
@@ -0,0 +1,5 @@
+// Convert all the strings to title caps in a string array 
+var a=['harish','sam','jack','amir'];
+var result=a.map(item)=>{return
+item[0].toUpperCase()+item.substr(1).toLowercase()};
+console.log(result);
