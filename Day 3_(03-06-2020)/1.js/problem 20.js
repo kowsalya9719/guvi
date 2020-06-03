3d67cc6d51d8664597c9d017a458abbbd9dc218f
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-04 00:46:52 +0530

    //problem 20

---
+++ Day 3_(03-06-2020)/1.js/problem 20.js
@@ -0,0 +1,11 @@
+Problem:
+Write a function called “getLastElement”.
+Given an array, “getLastElement” returns the last element of the given array. If the given array has a length of 0, it should return ‘-1’.
+Input:
+getLastElement([1, 2, 3, 4]);
+Output:
+4
+
+function getLastElement(array){
+ return array[length-1]
+}getLastElement([1, 2, 3, 4]);
