6b1227f7c2b6314ceec9d52016d7dc558f1b6f28
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 17:09:45 +0530

    //Parsing two JSON objects and Compare:

--- Day 2:(02-06-2020)/2.27 problem 7.js
+++ Day 2:(02-06-2020)/2.27 problem 7.js
@@ -1,17 +1,17 @@
  //Parsing two JSON objects and Compare:
    var expected = {foo: 5, bar: 6};
    var actual = {foo: 6, bar: 5}
-   function assertObjectsEqual(actual, expected, testName){
-  var act=JSON.stringify(actual);
-  var expect=JSON.stringify(expected);
-  var testCase=testName;
-  if(act===expect)
+   function assertObjectsEqual(actual, expected, test){
+  var actual1=JSON.stringify(actual);
+  var expect1=JSON.stringify(expected);
+  var testCase=test;
+  if(actual1===expect1)
    {
      console.log("Passed");
    }
     else
     {
-     console.log("FAILED [my test] Expected"+expect+"but got"+act)
+     console.log("FAILED [my test] Expected"+expect1+"but got"+actual1)
     }
     }
      assertObjectsEqual(actual,expected,"case1")
