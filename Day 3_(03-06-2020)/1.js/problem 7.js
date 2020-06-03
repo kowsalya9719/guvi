40cf33d5dc1335537f21fac932a5c7763954a4e6
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 23:49:12 +0530

    //problem 7

---
+++ Day 3_(03-06-2020)/1.js/problem 7.js
@@ -0,0 +1,14 @@
+Problem
+Convert Hours into Seconds
+Write a function that converts hours into seconds.
+Examples
+hourToSeconds(2) ➞ 7200
+hourToSeconds(10) ➞ 36000
+hourToSeconds(24) ➞ 86400
+
+var arr = [1, 2, 3];
+function hourToSeconds(arr) {
+return newarr=arr.map(res => res * 60);
+}
+var data = hourToSeconds(arr)
+console.log(data);
