3044135767444a045ac94d956f07a6916a2ffcbf
kowsalya9719 <60962377+kowsalya9719@users.noreply.github.com>
2020-06-03 23:36:05 +0530

    //program 3

---
+++ Day 3_(03-06-2020)/1.js/Program 3.js
@@ -0,0 +1,13 @@
+Problem:
+Fill in your code that takes an number minutes and converts it to seconds.
+Examples
+toSeconds(5) ➞ 300
+toSeconds(3) ➞ 180
+toSeconds(2) ➞ 120
+
+var min = 5;
+function toSeconds(min) {
+return min*60
+}
+var secs = toSeconds(min)
+console.log(secs);
